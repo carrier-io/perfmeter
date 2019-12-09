f55ede36347d80d645105e53eb6add0c2a07656b
Mykhailo Hunko <mykhailo_hunko@epam.com>
2019-12-09 19:22:51 +0200

    exception handling

--- pre_processing/minio_additional_files_reader.py
+++ pre_processing/minio_additional_files_reader.py
@@ -4,14 +4,14 @@ from traceback import format_exc
 import json
 
 URL = environ.get('galloper_url')
-ADDITIONAL_FILES = json.loads(environ.get("additional_files"))
+ADDITIONAL_FILES = environ.get("additional_files")
 
 if not all(a for a in [URL, ADDITIONAL_FILES]):
     exit(0)
 
 try:
-
-    for file, path in ADDITIONAL_FILES.items():
+    files = json.loads(ADDITIONAL_FILES)
+    for file, path in files.items():
         r = requests.get(f'{URL}/artifacts/{file}', allow_redirects=True)
         with open(path, 'wb') as file_data:
             file_data.write(r.content)
