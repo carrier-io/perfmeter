7fdb584f03318f376de1924d1e403c2b91913ef0
Mykhailo Hunko <mykhailo_hunko@epam.com>
2020-03-24 14:35:07 +0200

    Added project_id to artifacts API

--- pre_processing/minio_tests_reader.py
+++ pre_processing/minio_tests_reader.py
@@ -3,6 +3,7 @@ import requests
 import zipfile
 from traceback import format_exc
 
+PROJECT_ID = environ.get('project_id')
 URL = environ.get('galloper_url')
 BUCKET = environ.get("bucket")
 TEST = environ.get("artifact")
@@ -13,7 +14,12 @@ if not all(a for a in [URL, BUCKET, TEST]):
     exit(0)
 
 try:
-    r = requests.get(f'{URL}/artifacts/{BUCKET}/{TEST}', allow_redirects=True)
+    if PROJECT_ID:
+        endpoint = f'/api/v1/artifacts/{PROJECT_ID}/{BUCKET}/{TEST}'
+    else:
+        endpoint = f'/artifacts/{BUCKET}/{TEST}'
+
+    r = requests.get(f'{URL}/{endpoint}', allow_redirects=True)
     with open(PATH_TO_FILE, 'wb') as file_data:
         file_data.write(r.content)
     with zipfile.ZipFile(PATH_TO_FILE, 'r') as zip_ref:
